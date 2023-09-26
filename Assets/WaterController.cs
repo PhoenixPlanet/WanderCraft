d4e3107c9882b6f11ebb9fedb0c64ff61b49b594
HANGHAE <109887066+swimmin99@users.noreply.github.com>
2023-09-26 23:51:18 +0900

    [Feat] WaterController

--- Assets/WaterController.cs
+++ Assets/WaterController.cs
@@ -1,16 +1,25 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class WaterController : MonoBehaviour
 {
     #region PublicVariables
     
-    public float _initialSpeed = 0.01f;
-    public float _acceleration = 0.01f;
-    public float _maxSpeed = 0.1f; // Maximum speed
-    public float _stageDuration = 10f;
-
+    public enum WaveStatus
+    {
+       Idle,
+       Low,
+       Middle,
+       High
+    }
+    WaveStatus _waveStatus;
+    public float _Speed = 0.1f;
+    public Stack<WaveStatus> _waveStatusForecast;
+    public WaveStatus _currentWaveStatus;
     #endregion
 
     #region PrivateVariables
@@ -19,10 +28,20 @@ public class WaterController : MonoBehaviour
     #region PublicMethod
 
     private GameObject _waterGroup;
-    private float _startTime;
-    private float _stageEndTime;
-    private float _currentSpeed;
-
+    [Header("IdleState")]
+    [SerializeField] private float _IdleWaveHeight;
+    [SerializeField] private float _IdleWaveTime;
+    [Header("LowState")]
+    [SerializeField] float _LowWaveHeight;
+    [SerializeField] private float _LowWaveTime;
+    [Header("MiddleState")]
+    [SerializeField] private float _MiddleWaveHeight;
+    [SerializeField] private float _MiddleWaveTime;
+    [Header("HighState")]
+    [SerializeField] private float _HighWaveHeight;
+    [SerializeField] private float _HighWaveTime;
+    private int CycleCount = 0;
+    private bool _isExecutingCycle = false;
     #endregion
 
     #region PrivateMethod
@@ -30,25 +49,114 @@ public class WaterController : MonoBehaviour
     private void Start()
     {
         _waterGroup = GameObject.FindGameObjectWithTag("Water");
-        _startTime = Time.time;
-        _stageEndTime = _startTime + _stageDuration;
-        _currentSpeed = _initialSpeed;
+        _waveStatusForecast = new Stack <WaveStatus>();
+        setRandomStatusOrder();
+        StartCoroutine(IE_totalCycle());
+    
     }
 
-    private void Update()
+    IEnumerator IE_totalCycle()
     {
-        float _elapsedTime = Time.time - _startTime;
-
-        if (Time.time > _stageEndTime)
+        while(true)
         {
-            _startTime = Time.time;
-            _stageEndTime = _startTime + _stageDuration;
-            _currentSpeed = Mathf.Min(_currentSpeed + _acceleration, _maxSpeed);
+            if (_isExecutingCycle == false)
+            {
+            nextState();
+
+            }
+            yield return null;
         }
-
-        float _newYPos = _waterGroup.transform.position.y + _currentSpeed * Time.deltaTime;
-
-        _waterGroup.transform.position = new Vector3(_waterGroup.transform.position.x, _newYPos, _waterGroup.transform.position.z);
     }
+
+    private IEnumerator IdleWaveCoroutine()
+    {
+        yield return new WaitForSeconds(_IdleWaveTime);
+        _isExecutingCycle = false;
+    }
+
+    private IEnumerator LowWaveCoroutine()
+    {
+        riseSeaLevel(_LowWaveHeight + CycleCount / 3 * 4);
+        yield return new WaitForSeconds(_LowWaveTime);
+        lowerSeaLevel(_IdleWaveHeight);
+        yield return null;
+        _isExecutingCycle = false;
+    }
+
+    private IEnumerator MiddleWaveCoroutine()
+    {
+        riseSeaLevel(_MiddleWaveHeight + CycleCount / 3 * 4);
+        yield return new WaitForSeconds(_MiddleWaveTime);
+        lowerSeaLevel(_MiddleWaveHeight);
+        yield return null;
+        _isExecutingCycle = false;
+    }
+
+    private IEnumerator HighWaveCoroutine()
+    {
+       riseSeaLevel(_HighWaveHeight + CycleCount / 3 * 4);
+       yield return new WaitForSeconds(_HighWaveTime);
+       lowerSeaLevel(_HighWaveHeight);
+        yield return null;
+        _isExecutingCycle = false;
+    }
+
+
+    void riseSeaLevel (float level)
+    {
+        _waterGroup.transform.DOMoveY(level, 1).SetEase(Ease.InSine);
+    }
+
+    void lowerSeaLevel(float level)
+    {
+        _waterGroup.transform.DOMoveY(level, 1).SetEase(Ease.InSine);
+    }
+
+    private void setRandomStatusOrder()
+    {
+        _waveStatusForecast.Push(WaveStatus.Idle);
+        for(int i = 0; i<10; i++)
+        {
+            WaveStatus waveStatus;
+            int numValues = System.Enum.GetValues(typeof(WaveStatus)).Length;
+            int randomIndex = Random.Range(1, numValues);
+            waveStatus = (WaveStatus)randomIndex;
+            _waveStatusForecast.Push(waveStatus);
+        }
+    }
+
+    private void nextState()
+    {
+        _isExecutingCycle = true;
+        if (_waveStatusForecast.Count == 0)
+        {
+            CycleCount++;
+            setRandomStatusOrder();
+        }
+        WaveStatus nextWaveStatus = _waveStatusForecast.Pop();
+        _currentWaveStatus = nextWaveStatus;
+        print(_currentWaveStatus);
+
+        switch (nextWaveStatus)
+        {
+            case WaveStatus.Idle:
+                StartCoroutine(IdleWaveCoroutine());
+                break;
+            case WaveStatus.Low:
+                StartCoroutine(LowWaveCoroutine());
+                break;
+            case WaveStatus.Middle:
+                StartCoroutine(MiddleWaveCoroutine());
+
+                break;
+            case WaveStatus.High:
+                StartCoroutine(HighWaveCoroutine());
+
+                break;
+        }
+    }
+
+
     #endregion
 }
+
