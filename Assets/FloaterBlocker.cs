3ed84f79523cfb61e2dcc8838fca68679ba0346c
HANGHAE <109887066+swimmin99@users.noreply.github.com>
2023-09-26 16:52:36 +0900

    [Feat] spawner, flying, flow

---
+++ Assets/FloaterBlocker.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TH.Core;
+using UnityEngine;
+
+public class FloaterBlocker : MonoBehaviour
+{
+    #region PublicVariables
+    #endregion
+
+    #region PrivateVariables
+    private float _sphereRadius;
+    private SphereCollider _sphereCollider;
+    #endregion
+
+    #region PublicMethod
+    #endregion
+
+    #region PrivateMethod
+    private void Start()
+    {
+        _sphereRadius = GridManager.Instance.GridSize.x;
+        _sphereCollider = GetComponent<SphereCollider>();
+        _sphereCollider.radius = _sphereRadius;
+    }
+
+
+
+
+    #endregion
+}
