75862922364e545dbf8f9eb23d0c3d4a1f7118fd
김우찬 <woochankim0303@gmail.com>
2023-10-03 21:56:48 +0900

    Editor: DataManager
    SO 통합 관리 도구

---
+++ Assets/Scripts/Editor/Legacy/DataManager.cs
@@ -0,0 +1,39 @@
+﻿using Sirenix.OdinInspector.Editor;
+using System;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using TH.Core;
+using UnityEngine;
+
+namespace Editor.Legacy
+{
+public class DataManager : OdinMenuEditorWindow
+{
+    private static Type[] typesToDisplay = TypeCache.GetTypesWithAttribute<ManageableDataAttribute>()
+        .OrderBy(m => m.Name)
+        .ToArray();
+
+    private Type selectedType;
+
+    [MenuItem("Tools/Data Manager")]
+    private static void OpenEditor() => GetWindow<DataManager>();
+
+    protected override void OnGUI()
+    {
+        // draw menu tree for SOs and other assets
+        if (EditorGUIUtils.SelectButtonList(ref selectedType, typesToDisplay))
+            ForceMenuTreeRebuild();
+        
+        base.OnGUI();
+    }
+    
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        var tree = new OdinMenuTree();
+        tree.AddAllAssetsAtPath(selectedType.Name, "Assets/", selectedType, true, true);
+
+        return tree;
+    }
+}
+}
